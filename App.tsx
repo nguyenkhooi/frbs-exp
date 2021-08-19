f6a332a8a26dc294b7f506e1659328b4ad218cbf
nguyenkhooi <drkhoi15@hotmail.com>
2021-08-19 14:27:47 +0700

    M setup frbs-exp

--- App.tsx
+++ App.tsx
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import { FontAwesome, FontAwesome5, Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import * as React from "react";
+import { Text, View } from "react-native";
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      {Array.from({ length: 100 }).map((index) => (
+        <FontAwesome5 key={index} name="check" />
+      ))}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+function App() {
+  const [isReady, setIsReady] = React.useState(false);
+  async function _loadAssetsAsync() {
+    // const imageAssets = cacheImages([
+    //   "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+    //   require("./assets/images/circle.jpg"),
+    // ]);
+
+    const fontAssets = cacheFonts([
+      FontAwesome.font,
+      FontAwesome5.font,
+      Ionicons.font,
+    ]);
+
+    await Promise.all([...fontAssets]);
+  }
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={_loadAssetsAsync}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function cacheFonts(fonts) {
+  return fonts.map((font) => Font.loadAsync(font));
+}
+
+export default App;
